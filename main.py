424f657d1dffd1406ffd47c9be0612122f412178
kperkns2 <kap20k4@gmail.com>
2023-04-11 14:14:08 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -1,98 +1,31 @@
 import streamlit as st
-import pyaudio
-import io
+from streamlit import components
 import os
-from google.cloud import speech_v1p1beta1 as speech
-from google.cloud import texttospeech_v1 as tts
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'path/to/your/credentials.json'
+_RELEASE = False
 
-# Initialize the Google Speech client
-speech_client = speech.SpeechClient()
-
-# Initialize the Google Text-to-Speech client
-tts_client = tts.TextToSpeechClient()
-
-
-def record_audio():
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    RECORD_SECONDS = 5
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    return b''.join(frames)
-
-
-def transcribe_audio(audio_data):
-    audio = speech.RecognitionAudio(content=audio_data)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="en-US",
+if _RELEASE:
+    _component_func = components.declare_component(
+        "speech_to_text_component",
+        url="https://your-deployed-component-url",
     )
-
-    response = speech_client.recognize(config=config, audio=audio)
-
-    return response.results[0].alternatives[0].transcript
+else:
+    _component_func = components.declare_component("speech_to_text_component", path="speech_to_text_component")
 
 
-def synthesize_speech(text):
-    input_text = tts.SynthesisInput(text=text)
-    voice = tts.VoiceSelectionParams(
-        language_code="en-US", ssml_gender=tts.SsmlVoiceGender.FEMALE
-    )
-    audio_config = tts.AudioConfig(
-        audio_encoding=tts.AudioEncoding.MP3
-    )
-
-    response = tts_client.synthesize_speech(
-        input=input_text, voice=voice, audio_config=audio_config
-    )
-
-    return response.audio_content
+def speech_to_text(key=None):
+    return _component_func(key=key)
 
 
-def play_audio(audio_data):
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(2),
-                    channels=1,
-                    rate=24000,
-                    output=True)
-    stream.write(audio_data)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+if __name__ == "__main__":
+    st.set_page_config(page_title="Speech to Text", layout="wide")
+    st.title("Speech to Text")
 
+    with st.form(key="my_form"):
+        st.write("Press and hold the button while speaking:")
+        transcription = speech_to_text()
+        st.write("Transcription:", transcription)
 
-st.title("Streamlit Chat with Google STT and TTS")
-
-if st.button("Start conversation"):
-    st.write("Please speak for 5 seconds...")
-
-    audio_data = record_audio()
-    transcript = transcribe_audio(audio_data)
-
-    st.write(f"You said: {transcript}")
-
-    response_text = f"You said: {transcript}"
-    response_audio = synthesize_speech(response_text)
-    play_audio(response_audio)
+        submit_button = st.form_submit_button(label="Submit")
+        if submit_button:
+            st.write("You submitted:", transcription)
