7d343f025a592aabad5a96774f57225f5bc852bd
kperkns2 <kap20k4@gmail.com>
2023-04-11 16:06:16 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
-st.write("Note: This app uses the SpeechRecognition API, which might not work on some hosted environments.")
+stt_button = Button(label="Speak", width=100)
 
-stt_js = CustomJS(code="""
+stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
-
+ 
     recognition.onresult = function (e) {
         var value = "";
         for (var i = e.resultIndex; i < e.results.length; ++i) {
@@ -23,22 +23,16 @@ stt_js = CustomJS(code="""
         }
     }
     recognition.start();
-    """)
-
-# Create a Bokeh Button widget
-stt_button = Button(label="Speak", width=100)
-
-# Modify the button's attributes to trigger the JavaScript code
-stt_button.attributes["onclick"] = stt_js.code
+    """))
 
 result = streamlit_bokeh_events(
     stt_button,
     events="GET_TEXT",
     key="listen",
     refresh_on_update=False,
+    override_height=75,
     debounce_time=0)
 
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
-
