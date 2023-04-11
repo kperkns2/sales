f4eafecf644ceaeec054423227e4347d3cb765ed
kperkns2 <kap20k4@gmail.com>
2023-04-11 14:17:49 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st
-from streamlit import components
+from streamlit.components.v1 import declare_component
 import os
 
 _RELEASE = False
 
 if _RELEASE:
-    _component_func = components.declare_component(
+    _component_func = declare_component(
         "speech_to_text_component",
         url="https://your-deployed-component-url",
     )
 else:
-    _component_func = components.declare_component("speech_to_text_component", path="speech_to_text_component")
+    _component_func = declare_component("speech_to_text_component", path="speech_to_text_component")
 
 
 def speech_to_text(key=None):
