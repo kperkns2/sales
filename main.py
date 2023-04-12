9957702ed7a653fed3985b51ad4a16cbeb697a9d
kperkns2 <kap20k4@gmail.com>
2023-04-12 18:54:25 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ def get_audio_player(audio_data):
     return f'<audio autoplay visibility="hidden" controls src="data:audio/mp3;base64,{audio_base64}">'
 
 def text_to_speech(text):
+    
     tts = gTTS(text=text, lang='en')
     with NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_file:
         tts.save(tmp_file.name)
         audio_data = open(tmp_file.name, "rb").read()
-    return audio_data
+    audio_player = get_audio_player(audio_data)
+    st.write(audio_player, unsafe_allow_html=True)
 
 st.title('Text-to-Speech App')
 
 user_input = st.text_input('Enter text to be spoken:')
 if user_input:
-    audio_data = text_to_speech(user_input)
-    audio_player = get_audio_player(audio_data)
-    st.write(audio_player, unsafe_allow_html=True)
+    text_to_speech(user_input)
+    
+    
