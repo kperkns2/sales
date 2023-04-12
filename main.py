5be1a3f1d46145dc82048c7bccd5d9ef1886b0b8
kperkns2 <kap20k4@gmail.com>
2023-04-12 11:51:38 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -3,20 +3,41 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
-
-
 toggle_button = Button(label="Start Listening", width=100)
 
 toggle_button.js_on_event("button_click", CustomJS(code="""
-    console.log(this.label)
-    if (this.label == "Start Listening") {
+    var state_element = document.getElementById("state");
+
+    if (state_element.value === "start_listening") {
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onresult = function (e) {
+            var value = "";
+            for (var i = e.resultIndex; i < e.results.length; ++i) {
+                if (e.results[i].isFinal) {
+                    value += e.results[i][0].transcript;
+                }
+            }
+            if (value != "") {
+                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+            }
+        }
+        recognition.start();
+        state_element.value = "speak";
         this.label = "Speak";
-        console.log('a')
     } else {
-        this.label = "Start Listening"
-        console.log('b')
+        var u = new SpeechSynthesisUtterance();
+        u.text = "You said " + document.getElementById("user-text").innerText;
+        u.lang = 'en-US';
+        speechSynthesis.speak(u);
+        state_element.value = "start_listening";
+        this.label = "Start Listening";
     }
     """))
+
+st.write('<input type="hidden" id="state" value="start_listening">', unsafe_allow_html=True)
 
 result = streamlit_bokeh_events(
     toggle_button,
@@ -29,4 +50,4 @@ result = streamlit_bokeh_events(
 if result:
     if "GET_TEXT" in result:
         user_text = result.get("GET_TEXT")
-        st.write(f'<script>document.getElementById("user-text").innerText = "{user_text}";</script>', unsafe_allow_html=True)
+        st.write(f'<p id="user-text">{user_text}</p>', unsafe_allow_html=True)
