ed931651db81bdd174cfee492e8566c249ade65a
kperkns2 <kap20k4@gmail.com>
2023-04-12 11:47:22 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -3,36 +3,15 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
-st.write('<p id="user-text"></p>', unsafe_allow_html=True)
+
 
 toggle_button = Button(label="Start Listening", width=100)
 
 toggle_button.js_on_event("button_click", CustomJS(code="""
     if (this.label == "Start Listening") {
         this.label = "Speak";
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
-
-        recognition.onresult = function (e) {
-            var value = "";
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                    value += e.results[i][0].transcript;
-                }
-            }
-            if ( value != "") {
-                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-            }
-        }
-        recognition.start();
-        
     } else {
-        var u = new SpeechSynthesisUtterance();
-        u.text = "You said " + document.getElementById("user-text").innerText;
-        u.lang = 'en-US';
-        speechSynthesis.speak(u);
-        this.label = "Start Listening";
+        this.label = "Start Listening"
     }
     """))
 
