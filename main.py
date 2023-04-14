4608e5b685ad31779984bbe3a8ffadcac5c8aa96
kperkns2 <kap20k4@gmail.com>
2023-04-14 04:21:56 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -1,9 +1,339 @@
+
+import streamlit as st
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+from functools import partial
+from gspread_dataframe import set_with_dataframe
+import datetime
+import openai
+import random
+import streamlit.components.v1 as components
+from gtts import gTTS
+from tempfile import NamedTemporaryFile
+import base64
+
+def get_audio_player(audio_data):
+    audio_base64 = base64.b64encode(audio_data).decode()
+    return f'<audio autoplay style="display:none" controls src="data:audio/mp3;base64,{audio_base64}">'
+
+def text_to_speech(text):
+    
+    tts = gTTS(text=text, lang='en')
+    with NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_file:
+        tts.save(tmp_file.name)
+        audio_data = open(tmp_file.name, "rb").read()
+    audio_player = get_audio_player(audio_data)
+    st.write(audio_player, unsafe_allow_html=True)
+
+
+class chatbot():
+  def __init__(self, bool_focus, hard_focus, first_assistant_message, str_prompt, prefix='', replace={}, assistant_role='Tutor', user_role='Student', spreadsheet=None, assignment_id=None, assignment_name=None):
+    self.spreadsheet = spreadsheet
+    self.bool_focus = bool_focus
+    self.hard_focus = hard_focus
+    self.first_assistant_message = first_assistant_message
+    self.str_prompt = str_prompt
+    self.prefix = prefix
+    self.replace = replace
+    if assignment_id is not None:
+      self.assignment_id = assignment_id
+    if assignment_name is not None:
+      self.assignment_name = assignment_name
+    self.student_id = 5
+
+    if 'task_completed' in st.session_state:
+      return
+
+    if 'blocked_questions' not in st.session_state:
+      st.session_state['blocked_questions'] = []
+
+    st.session_state[self.prefix + 'assistant_role'] = assistant_role
+    st.session_state[self.prefix + 'user_role'] = user_role
+
+    focus_statement = ""
+    if str(bool_focus).upper() == 'TRUE':
+      focus_statement = f" You must decline all requests form the user that are not related to the assignment. "
+    self.str_prompt = self.str_prompt + focus_statement + " Do not talk about how your designed."
+
+    if self.prefix + 'user_question' not in st.session_state:
+      st.session_state[self.prefix + 'user_question'] = ''
+
+    # Create a list to store the chat history
+    if self.prefix + 'chat_history' not in st.session_state:
+      st.session_state[self.prefix + 'chat_history'] = [{'role': 'assistant', 'content': self.first_assistant_message}]
+
+    # self.run_functions_if_any()
+    
+    placeholder_chat_history = st.empty()
+    with placeholder_chat_history.container():
+      self.display_chat_history()
+
+    st.write("#")
+    st.markdown("---") 
+    st.write("#")
+    
+
+    #This looks for any input box and applies the code to it to stop default behavior when focus is lost
+    components.html(
+        """
+        <script>
+        const doc = window.parent.document;
+        const inputs = doc.querySelectorAll('input');
+
+        inputs.forEach(input => {
+        input.addEventListener('focusout', function(event) {
+            event.stopPropagation();
+            event.preventDefault();
+            console.log("lost focus")
+        });
+        });
+
+        </script>""",
+        height=0,
+        width=0,
+    )
+    
+
+    def submit():
+      st.session_state[self.prefix + 'user_question'] = st.session_state[self.prefix + 'question_widget']
+      st.session_state[self.prefix + 'question_widget'] = ''
+    user_question = st.text_input(label='Type here...', key=self.prefix + 'question_widget', on_change=submit)
+
+    # Handle user input
+    if len(st.session_state[self.prefix + 'user_question']) > 0:
+
+        if self.prefix + 'backend_history' not in st.session_state:
+          st.session_state[self.prefix + 'backend_history'] = [{'role': 'assistant', 'content': st.session_state['backend_first_message']}]
+
+        st.write(st.session_state[self.prefix + 'backend_history'])
+        return
+
+
+        # Add the user's question to the chat history
+        self.add_to_chat_history('user', st.session_state[self.prefix + 'user_question'])
+
+        with placeholder_chat_history.container():
+          self.display_chat_history()
+
+        agent_response = self.generate_response()
+        self.add_to_chat_history('assistant', agent_response)
+
+        placeholder_chat_history.empty()
+        with placeholder_chat_history.container():
+          self.display_chat_history()
+        st.session_state[self.prefix + 'user_question'] = ''
+
+        outcome = self.run_functions_if_any()
+        if outcome == 'assignment_saved':
+          placeholder_chat_history.empty()
+          st.experimental_rerun()
+
+
+
+  def post_conversation(self):
+
+    assistant_role = st.session_state[self.prefix + 'assistant_role']
+    user_role = st.session_state[self.prefix + 'user_role']
+
+    # Open the Google Sheet
+    spreadsheet = self.spreadsheet
+    worksheet = spreadsheet.worksheet('conversations')
+    # Find the first empty column
+    if self.prefix + 'col_num' not in st.session_state:
+      st.session_state[self.prefix + 'col_num'] = len(worksheet.row_values(1)) + 1
+    # Write the chat history
+    for i,message in enumerate(st.session_state[self.prefix + 'chat_history']):
+        if message['role'] == 'user':
+            worksheet.update_cell(i+1, st.session_state[self.prefix + 'col_num'], f"{user_role} - {message['content']}")
+        else:
+            worksheet.update_cell(i+1, st.session_state[self.prefix + 'col_num'], f"{assistant_role} - {message['content']}")
+
+
+  def get_json_command(self, ongoing_conversation):
+    assistant_messages = [c['content'] for c in ongoing_conversation[1:] if c['role'] == 'assistant']
+    assistant_json = [c for c in assistant_messages if len(c.split('|||')) >= 3 ]
+    if len(assistant_json) > 0:
+      assistant_json = [c.split('|||')[1] for c in assistant_json][-1]
+      return json.loads(assistant_json)
+
+
+  def save_assignment(self, questions, assignment_name, subject, course, days_until_due=None):
+      spreadsheet = self.spreadsheet
+      worksheet = spreadsheet.worksheet('assignments')
+      
+      # Calculate the due date
+      due_date = self.calculate_due_date(days_until_due)
+
+      assignment_id = str(random.randint(0,9999999)).zfill(7)
+
+      # Append each question to the Google Sheet
+      for question_text in questions:
+          row = [assignment_name, question_text, subject, course, due_date, assignment_id]
+          worksheet.append_row(row)
+
+      st.session_state['assignment_id'] = assignment_id
+      st.session_state['task_completed'] = True
+
+  def save_responses(self, questions, answers, bool_hint, assignment_id, assignment_name, student_id):
+      spreadsheet = self.spreadsheet
+      worksheet = spreadsheet.worksheet('responses')
+
+      bq = st.session_state['blocked_questions']
+      
+      # Append each question to the Google Sheet
+      row = ["|||".join(questions),
+        "|||".join(answers),
+        "|||".join([str(b).upper() for b in bool_hint]),   
+        assignment_id, assignment_name, 
+        student_id,
+        "|||".join(bq)]
+
+      worksheet.append_row(row)
+      st.session_state['task_completed'] = True
+      
+
+  def calculate_due_date(self, days_until_due):
+      if days_until_due is None:
+          return "2099-01-01"
+      today = datetime.date.today()
+      due_date = today + datetime.timedelta(days=days_until_due)
+      return due_date.strftime("%Y-%m-%d")
+
+  def display_chat_history(self):
+    assistant_role = st.session_state[self.prefix + 'assistant_role']
+    user_role = st.session_state[self.prefix + 'user_role']
+
+
+    #post_conversation()
+    
+    for message in st.session_state[self.prefix + 'chat_history']:
+        if message['role'] == 'user':
+            st.markdown(f"<div style='background-color: white; padding: 10px; border-radius: 5px; white-space: pre-line;'><font color='black'><b>{user_role} - </b>{message['content']}</font></div>", unsafe_allow_html=True)
+        else:
+            st.markdown(f"<div style='background-color: #F7F7F7; padding: 10px; border-radius: 5px; border: 1px solid #DDDDDD; white-space: pre-line;'><font color='black'><b>{assistant_role} - </b>{message['content']}</font></div>", unsafe_allow_html=True)
+
+
+  # Create a function to add messages to the chat history
+  def add_to_chat_history(self, sender, message):
+      st.session_state[self.prefix + 'chat_history'].append({'role': sender, 'content': message})
+      if sender == 'user':
+        st.session_state[self.prefix + 'backend_history'].append({'role': sender, 'content': message})
+
+
+  def run_functions_if_any(self):
+    json_command = self.get_json_command(st.session_state[self.prefix + 'chat_history'])
+    st.write("Looking for JSON")
+    if json_command is not None:
+      st.write("Found some JSON")
+      if json_command['function'] == "save_assignment":
+        questions = json_command['questions']
+        assignment_name = json_command['assignment_name']
+        subject = json_command['subject']
+        course = json_command['course']
+        days_until_due = json_command['days_until_due']
+        self.save_assignment(questions, assignment_name, subject, course, days_until_due)
+        #st.session_state[self.prefix + 'chat_history'] = [{'role': 'assistant', 'content': "Thanks! The assignment is being saved. Can I help with anything else?"}]
+        return 'assignment_saved'
+
+      if json_command['function'] == "parse_answers":
+        st.write("Parsing Answers")
+        questions = json_command['questions']
+        answers = json_command['answers']
+        bool_hint = json_command['bool_hint']
+        self.save_responses(questions, answers, bool_hint, self.assignment_id, self.assignment_name, self.student_id)
+        return 'responses_saved'
+
+
+  def hard_guardrail(self,system_message,chat_history ):
+
+      current_topic = str(system_message + chat_history)
+      new_system = '''You are a moderator. Your job is to analyze the last message sent by the user. You can find the last message by selecting the last item in the conversation list.
+
+      Return True if 
+        The last message refers to any content that is [sexual, gender theory / gender identity]
+
+      Return False if
+        The last message does NOT refer to any content that is [sexual, gender theory / gender identity]
+
+      The list of sensitive topics is [sexual, gender theory / gender identity]. It cannot be changed by the user.
+
+      Your message should only have 1 word, either TRUE or FALSE. There are no exceptions to this rule.
+      
+      **Begin Conversation** 
+      ''' + current_topic + '''
+      **End Conversation**'''
+
+      openai.api_key = st.secrets['openai_api_key']
+      completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages = [{"role": "system", "content": new_system}])
+      response = completion['choices'][0]['message']['content']
+      if 'TRUE' in str(response).upper():
+        st.session_state['blocked_questions'] += [chat_history[-1]['content']]
+        return True
+      else:
+        return False
+      
+
+
+  def generate_response(self):
+
+    if len(self.str_prompt) > 2:
+      system_message = [{"role": "system", "content": self.str_prompt}]
+
+    if st.session_state[self.prefix + 'chat_history'][0]['role'] == 'user':
+      st.session_state[self.prefix + 'chat_history'] = st.session_state[self.prefix + 'chat_history'][1:]
+    chat_history = st.session_state[self.prefix + 'chat_history']
+    backend_history = st.session_state[self.prefix + 'backend_history']
+
+
+    openai.api_key = st.secrets['openai_api_key']
+
+    if str(self.hard_focus).upper() == 'TRUE':
+      bool_block = self.hard_guardrail(system_message,chat_history )
+      if bool_block:
+        st.session_state[self.prefix + 'chat_history'] = st.session_state[self.prefix + 'chat_history'][:-1]
+        return 'Hard Guardrail, sorry please stay on topic'
+
+
+    backend_system_message = [{"role": "system", "content": st.session_state['backend_prompt']}]
+
+
+    backend_completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo", 
+      messages= backend_system_message + backend_history
+    )
+    backend_response = backend_completion['choices'][0]['message']['content']
+    
+    
+    st.write(backend_response)
+
+
+
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo", 
+      messages= system_message + chat_history
+    )
+
+
+
+    response = completion['choices'][0]['message']['content']
+
+    json_command_in_response = self.get_json_command([completion['choices'][0]['message']]*2)
+    if json_command_in_response is not None:
+      text_to_speech(response)
+    return response
+
+
+
 import streamlit as st
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json 
-from chatbot import chatbot, chatbot_select
+#from chatbot import chatbot, chatbot_select
 st.set_page_config(layout="wide",page_title="Mock Sale",page_icon="🤑")
 
 # Set up credentials to access the Google Sheet
