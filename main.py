d74db4a11ed5b4f4649d9954d047123737e9b0a2
kperkns2 <kap20k4@gmail.com>
2023-04-30 02:18:16 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -10,16 +10,28 @@ def text_to_speech(text):
         with open(fp.name + '.mp3', 'rb') as f:
             audio_data = f.read()
         audio_base64 = base64.b64encode(audio_data).decode()
-        audio_tag = f'<audio controls autoplay><source src="data:audio/mp3;base64,{audio_base64}" type="audio/mpeg" /></audio>'
+        audio_tag = f'<audio id="response_audio" style="display:none;"><source src="data:audio/mp3;base64,{audio_base64}" type="audio/mpeg" /></audio>'
         st.markdown(audio_tag, unsafe_allow_html=True)
+        st.markdown(
+            """
+            <script>
+                function playAudio() {
+                    const audio = document.getElementById('response_audio');
+                    audio.play();
+                }
+            </script>
+            """,
+            unsafe_allow_html=True,
+        )
 
 st.set_page_config(page_title="Chat App", page_icon=":speech_balloon:")
 
 st.title("Chat Application")
 
 user_input = st.text_input("Enter your message:")
-send_button = st.button("Send")
+send_button = st.button("Send", on_click=text_to_speech, args=[user_input])
 
 if send_button and user_input:
     response = "This is the chatbot's response to: " + user_input
     text_to_speech(response)
+    st.markdown("<script>playAudio();</script>", unsafe_allow_html=True)
