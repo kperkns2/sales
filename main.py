b50db1f44ba8740d451f259ec8f91184a9d28a1c
kperkns2 <kap20k4@gmail.com>
2023-05-01 13:46:21 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ js_code = """
 
 if st.button("Speak"):
     # Call the speak function with the user input
-    st.markdown(f"<script>console.log("Hello world");</script>", unsafe_allow_html=True)
+    st.markdown(f"<script>console.log('Hello world');</script>", unsafe_allow_html=True)
     st.markdown(js_code, unsafe_allow_html=True)
     st.markdown(f"<script>speak('You said {user_input}')</script>", unsafe_allow_html=True)
