8b60fec5cd2af1b78c34922e880dc3b59d748197
kperkns2 <kap20k4@gmail.com>
2023-05-01 13:52:56 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ if st.button("Speak"):
     # Call the speak function with the user input
     js_code = f"""
     <script>
-            console.log("speak() called with:", {user_input});
-            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-            const utterance = new SpeechSynthesisUtterance({user_input});
-            utterance.onstart = function (event) {{
-                audioContext.resume();
-            }};
-            window.speechSynthesis.speak(utterance);
+    console.log("speak() called with:", {user_input});
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const utterance = new SpeechSynthesisUtterance({user_input});
+    utterance.onstart = function (event) {{
+    audioContext.resume();
+    }};
+    window.speechSynthesis.speak(utterance);
     </script>
     """
     html(js_code)
