089b0d139fe5537f7c0fdb5d310b0d3e0e25c77e
kperkns2 <kap20k4@gmail.com>
2023-05-01 13:39:12 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 import streamlit as st
-from gtts import gTTS
-import tempfile
-import base64
 
-def text_to_speech(text):
-    tts = gTTS(text, lang='en')
-    with tempfile.NamedTemporaryFile(delete=True) as fp:
-        tts.save(fp.name + '.mp3')
-        with open(fp.name + '.mp3', 'rb') as f:
-            audio_data = f.read()
-        audio_base64 = base64.b64encode(audio_data).decode()
-        audio_tag = f'<audio id="response_audio" style="display:none;"><source src="data:audio/mp3;base64,{audio_base64}" type="audio/mpeg" /></audio>'
-        st.markdown(audio_tag, unsafe_allow_html=True)
-        st.markdown(
-            """
-            <script>
-                function playAudio() {
-                    const audio = document.getElementById('response_audio');
-                    audio.play();
-                }
-            </script>
-            """,
-            unsafe_allow_html=True,
-        )
+st.title("Text to Speech POC")
 
-st.set_page_config(page_title="Chat App", page_icon=":speech_balloon:")
+user_input = st.text_input("Enter your text:")
 
-st.title("Chat Application")
+js_code = """
+<script>
+    function speak(text) {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const utterance = new SpeechSynthesisUtterance(text);
 
-user_input = st.text_input("Enter your message:")
-send_button = st.button("Send", on_click=text_to_speech, args=[user_input])
+        utterance.onstart = function (event) {
+            audioContext.resume();
+        };
 
-if send_button and user_input:
-    response = "This is the chatbot's response to: " + user_input
-    text_to_speech(response)
-    st.markdown("<script>playAudio();</script>", unsafe_allow_html=True)
+        window.speechSynthesis.speak(utterance);
+    }
+</script>
+"""
+
+st.markdown(js_code, unsafe_allow_html=True)
+
+if st.button("Speak"):
+    # Call the speak function with the user input
+    st.markdown(f"<script>speak('You said {user_input}')</script>", unsafe_allow_html=True)
