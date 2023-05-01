7a2aa53d9f7c8f6f091ebdfc9d0c9cdb1d22a213
kperkns2 <kap20k4@gmail.com>
2023-05-01 13:47:34 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import html
 
 st.title("-- Text to Speech POC --")
 
@@ -24,6 +25,6 @@ js_code = """
 
 if st.button("Speak"):
     # Call the speak function with the user input
-    st.markdown(f"<script>console.log('Hello world');</script>", unsafe_allow_html=True)
+    html(f"<script>console.log('Hello world');</script>")
     st.markdown(js_code, unsafe_allow_html=True)
     st.markdown(f"<script>speak('You said {user_input}')</script>", unsafe_allow_html=True)
